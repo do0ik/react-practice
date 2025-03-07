14c64fe068be884656e741d0fcd463f49ad06f97
do0ik <oulwind00@naver.com>
2025-03-08 02:32:04 +0900

    Complete 7-6 sample codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/hello" element={<h1>Hello</h1>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
